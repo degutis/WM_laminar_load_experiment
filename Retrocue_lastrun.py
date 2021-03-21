581d4eb33afa2b68027ba8ac4cb6e0e5c54ff75f
Karolis Degutis <degutis@nilpferd.cbs.mpg.de>
2021-03-21 18:21:22 +0100

    trial update

--- Retrocue_lastrun.py
+++ Retrocue_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on Thu 18 Mar 2021 11:26:29 AM CET
+    on Fri 19 Mar 2021 01:45:00 PM CET
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
